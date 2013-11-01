00a24b13a68136f3f50b1c1cd987fe01a9b889d7
Michele Bini <michele.bini@gmail.com>
2013-11-01 05:42:37 +0100

    Update CDLList.hpp
    Describe purpose a bit, prevent multiple inclusion of header file

--- CDLList.hpp
+++ CDLList.hpp
@@ -1,16 +1,23 @@
+// CDLList.hpp - Circular Doubly-Linked Lists
+
+// This is an header-only C++11 library
+
 // Copyright (c) 2013 Michele Bini
 
-// This program is free software: you can redistribute it and/or modify
+// CDLList is free software: you can redistribute it and/or modify
 // it under the terms of the version 3 of the GNU General Public License
 // as published by the Free Software Foundation.
 
-// This program is distributed in the hope that it will be useful, but
+// This library is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef __CDLLIST_HPP__
+#define __CDLLIST_HPP__
 
 class CDLList {
 
@@ -81,4 +88,4 @@ public:
   };
 };
 
-
+#endif
